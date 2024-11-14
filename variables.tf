6569d8becf5327363972a1b42eff8df73c1b20c5
acnoh <andreac.noh@gmail.com>
2024-11-13 21:12:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "anoh"
 }
 
 variable "class_name" {
