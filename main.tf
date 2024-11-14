9488c28f5b7d7e5f1344cee83fae5985bdd132d0
acnoh <andreac.noh@gmail.com>
2024-11-13 21:06:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,44 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+  tags = local.tags
+}
+
+// Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+
+
+}
+
+resource "azurerm_mssql_server" "sqlsrvr" {
+  name                = "sqlsrvr-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  version             = "12.0"
+  administrator_login  = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+  tags = local.tags
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sqlsrvr_vnet" {
+  name      = "sqlsrvr_vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  server_id = azurerm_mssql_server.sqlsrvr.id
+  subnet_id = azurerm_subnet.subnet.id
+
+}
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
@@ -42,3 +79,4 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
